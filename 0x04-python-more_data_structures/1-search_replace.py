ed725cbf0a72c42c2767a44401313fc7cdbfb4a1
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2020-12-17 14:29:49 -0500

    fixed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     my_list = [] #Creamos nuestra lista
-    for elem in my_list: #Recorremos el elemento iterable
-        if elemm == search: #El elemento va a ser igual a search que es el valor a remplazar
+    for num in my_list: #Recorremos el elemento iterable
+        if num == search: #El elemento va a ser igual a search que es el valor a remplazar
             new_list.append(replace)#Usamos .aprrend para agregar un valor a nuestra lista en este caso replace
         else:
-            new_list.append(elemt)
+            new_list.append(num)
     return new_list
