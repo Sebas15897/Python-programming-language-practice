c9fec5ae28aa393b6bc729ace7d47087104a81c0
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-01-09 15:21:54 -0500

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""
-a function that adds 2 integers
-"""
+""" A simple functino that adds 2 integers and returns the result """
 
 
 def add_integer(a, b=98):
-    """
-    Returns an integer: the addition of a and b
-    """
+    """ Given 2 variables, return the addition them """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
-        raise TypeError("a must be an integer")
+    elif type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+
     return int(a) + int(b)
