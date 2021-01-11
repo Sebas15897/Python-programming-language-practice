b7a779f1e5a0cad3cdc6bf349afe6601d177fd23
Sebas15897 <sebastiancontreras15897@gmail.com>
2021-01-11 23:10:38 +0000

    update

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-class Rectangle(object):
-                """docstring for Rentangle"""
+""" this is an empty class that defines a rectangle """
+
+
+class Rectangle():
+                """ docstring for Rentangle """
+                pass
