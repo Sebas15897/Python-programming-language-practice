c1e16f801d93f544c4f5d0e691e7456959259292
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-01-15 20:39:50 -0500

    fixed

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""Function to print two names"""
+"""
+    funtion to print two names
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """Edge cases for trying to print names
+    """
+    >>> tester = __import__('3-say_my_name').say_my_name
     """
     if type(first_name) is not str:
-        raise TypeError("first_name must be a string")
+        raise TypeError('first_name must be a string')
     if type(last_name) is not str:
-        raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+        raise TypeError('last_name must be a string')
+    print("My name is " + first_name + " " + last_name)
