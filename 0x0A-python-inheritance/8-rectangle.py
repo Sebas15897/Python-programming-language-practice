77cf2f150196b7bdd883c83f1e63a7558364180f
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-01-19 20:49:38 -0500

    fixxxxxxxxxxxxxxxxxxxxxxxxxxxx

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""module of rectangle"""
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+'''Rectangle module'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """class Rectangle that inherits from BaseGeometry"""
+    '''class Rectangle'''
     def __init__(self, width, height):
-        """initialization method"""
+        '''initialize constructor width and height'''
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
