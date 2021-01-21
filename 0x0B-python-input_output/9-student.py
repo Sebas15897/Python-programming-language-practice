a92fa96d2fd575887d1ad39acb8c585a83a16a71
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-01-20 23:12:04 -0500

    fix

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-""" This is a class that defines a student """
+""" Student to JSON """
 
 
-class Student():
-   """docstring for Student class"""
-  def __init__(self, first_name, last_name, age):
+class Student:
+    """ New class student """
+
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
