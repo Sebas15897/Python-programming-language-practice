eae71ba4593a59ab5318eddda035a979cf895b1a
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-01-26 08:43:59 -0500

    fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -16,16 +16,18 @@ class Rectangle(Base):
 	def width(self):
 		"""Width"""
 		return self._width
+
 	@property
 	def heigth(self):
 		"""Heigth"""
-		return self._heigth				
+		return self._heigth	
+
 	@property
 	def x(self):
 		"""X"""
 		return self._x
+
 	@property
 	def y(self):
 		"""Y"""
 		return self._y
-      
