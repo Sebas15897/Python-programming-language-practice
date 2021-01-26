a0ca0d6e32374a1797b346e39666401d920b1c54
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-01-26 09:04:32 -0500

    fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Initializes module and import Base """
+"""import"""
 from .base import Base
 
 
 class Rectangle(Base):
-    """ Class that inherits from super class Base"""
+    """doctrings for base"""
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ Initial state of new instances"""
+        """comments of atributes"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -15,27 +15,27 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """ Gets the width """
+        """width"""
         return self.__width
 
     @property
     def height(self):
-        """ Gets the height """
+        """height"""
         return self.__height
 
     @property
     def x(self):
-        """ Gets x """
+        """x"""
         return self.__x
 
     @property
     def y(self):
-        """ Gets y """
+        """y"""
         return self.__y
 
     @width.setter
     def width(self, value):
-        """ Sets the width """
+        """width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -44,7 +44,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        """ Sets the height """
+        """height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -53,7 +53,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        """ Sets x """
+        """Setter"""
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
@@ -62,7 +62,7 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        """ Sets the y """
+        """Setter"""
         if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
