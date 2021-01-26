78560c9e4b1fcd4fc109d9d2aca96467435714e8
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-01-26 08:42:09 -0500

    fix rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-"""import"""
+"""Import"""
 from models.base import Base
 
-
 class Rectangle(Base):
-      """docstring for class and super class"""
-      def __init__(self, width, height, x=0, y=0, id=None):
-            """instances"""
-            super().__init__(id)
-            self.width = width
-            self.heigth = height
-            self.x = x
-            self.y = y
+	"""docstring for Rectangle"""
+	def __init__(self, width, heigth, x, y):
+		super().__init__(id)
+		self.width = width
+		self.heigth = heigth
+		self.x = x
+		self.y = y
 
-      @property
-      def width(self):
-"""width"""
-                  return self._width
-            @property
-            def heigth(self):
-                  """heigth"""
-                  return self._heigth
-            @property
-            def x(self):
-                  """x"""
-                  return self._x
-            @property
-            def y(self):
-                  """y"""
-                  return self._y
+	@property
+	def width(self):
+		"""Width"""
+		return self._width
+	@property
+	def heigth(self):
+		"""Heigth"""
+		return self._heigth				
+	@property
+	def x(self):
+		"""X"""
+		return self._x
+	@property
+	def y(self):
+		"""Y"""
+		return self._y
+      
