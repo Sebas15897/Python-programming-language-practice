9b1cfce94e144b0a356b43c4e6b8cf0ee8d11d39
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-01-27 10:18:05 -0500

    fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,6 +8,11 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         "Initiation of Rectangle"
         super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+   
         if type(width) is not int:
             raise TypeError('width must be an integer')
         if type(height) is not int:
@@ -24,10 +29,7 @@ class Rectangle(Base):
             raise ValueError('x must be >= 0')
         if y < 0:
             raise ValueError('y must be >= 0')
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+       
 
     @property
     def width(self):
