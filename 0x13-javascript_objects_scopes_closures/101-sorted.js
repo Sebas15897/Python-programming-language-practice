620bbff1a70c5910967dd66edf844c5ae70afb90
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-03-23 17:02:16 -0500

    fixed advance task

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
-const filtered = Object.keys(dict).reduce(
-  (acc, k) => (acc[dict[k]] = [...(acc[dict[k]] || []), k], acc), {});
-console.log(filtered);
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
+  }
+}
+console.log(newDict);
