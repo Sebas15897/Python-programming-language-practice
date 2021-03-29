ffa45b8a081ba43b253cef44d975e6cdc117c2c0
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-03-28 19:07:08 -0500

    fix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
 """
-a script that takes in arguments and displays all values
-in the states table of hbtn_0e_0_usa
+Takes in arguments and displays all values in the table of database
+where name matches the argument. But this time, write one that is
+SAFE FROM MySQL INJECTIONS!
 """
-import sys
-import MySQLdb
-if __name__ == "__main__":
-    states = MySQLdb.connect(host="localhost",
-                             port=3306,
-                             user=sys.argv[1],
-                             passwd=sys.argv[2],
-                             db=sys.argv[3])
-    cur = states.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    for row in cur.fetchall():
-        if row[1] == sys.argv[4]:
-            print("{}".format(row))
-    cur.close()
-    states.close()
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """Connect to a MySQL server."""
+
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3],
+                         port=3306)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name = %s\
+    ORDER BY id ASC".format(argv[4],))
+    states = cursor.fetchall()
+    for state in states:
+        if state[1] == argv[4]:
+            print(state)
+    cursor.close()
+    db.close()
