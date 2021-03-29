7f886f6df8cbe79d6f93e6a6a138e3e4fb2f5ea3
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-03-28 19:30:56 -0500

    fix task - 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
 """
-script that prints all City objects from the database
+This script joins City and State tables
+and prints a list of all Cities with their
+state names, city names, and city ids.
 """
-from sys import argv
+
+
+import sys
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import update
 
 if __name__ == "__main__":
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(argv[1], argv[2],
-                                  argv[3]), pool_pre_ping=True)
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-
-    Session = sessionmaker()
-    Session.configure(bind=engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    query = session.query(State, City).filter(State.id == City.state_id).all()
-    for result in query:
-        print("{}: ({}) {}".
-              format(result.State.name, result.City.id, result.City.name))
-    session.close()
+
+    usa = session.query(City, State) \
+        .join(State, State.id == City.state_id) \
+        .order_by(City.id.asc()) \
+        .all()
+
+    for city in usa:
+        print("{}: ({}) {}".format(city.State.name,
+                                   city.City.id,
+                                   city.City.name))
