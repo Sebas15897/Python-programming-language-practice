4ebb62ec36dc02b02f7cf4b3a2ccd4dde5ea1045
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-03-28 19:31:42 -0500

    fix

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
 """
-City class - MySQLAlchemy module
+This module has one class:
+City - inherited from Base.
 """
-from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+from model_state import State
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+Base = declarative_base()
 
 
 class City(Base):
     """
-    City class
+    This class is linked to the cities table.
+    Attributes:
+        id: Integer, required, primary key
+        name: String, required
+        state_id: Integer, Foreign Key to states.id
     """
-
     __tablename__ = 'cities'
-    id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    state = relationship("State", back_populates="cities")
+
+    id = Column(Integer,
+                primary_key=True)
+    name = Column(String(128),
+                  nullable=False)
+    state_id = Column(Integer,
+                      ForeignKey(State.id))
