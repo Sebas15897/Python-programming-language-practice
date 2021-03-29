2fc0f1c45035ecb918b44052db0e48c036d47f0e
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-03-28 19:04:27 -0500

    fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """
-script that takes in an argument and 
-displays all values in the 
-states table of hbtn_0e_0_usa where 
-name matches the argument.
+a script that takes in an argument and displays all
+values in the states table of hbtn_0e_0_usa
 """
 import sys
 import MySQLdb
