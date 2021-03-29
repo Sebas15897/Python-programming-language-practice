764581cec3546a52dffd7a70f53ff55ef7a4497d
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-03-28 19:03:19 -0500

    rix task - 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""Import MySQLdb"""
+"""
+script that takes in an argument and displays all values in the 
+states table of hbtn_0e_0_usa where name matches the argument.
+"""
+import sys
 import MySQLdb
-from sys import argv
-
 if __name__ == "__main__":
-    """
-    Script that lists all states from the database hbtn_0e_0_usa
-    """
-db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3], port=3306,
-                charset="utf8")
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name = '{}'\
-    ORDER BY states.id ASC".format(argv[4]))
-states = cur.fetchall()
-for i in states:
-    if i[1] == argv[4]:
-        print(i)
-cur.close()
-db.close()
+    states = MySQLdb.connect(host="localhost",
+                             port=3306,
+                             user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
+    cur = states.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cur.fetchall():
+        if row[1] == sys.argv[4]:
+            print("{}".format(row))
+    cur.close()
+    states.close()
