598b657abe59e48be43b99e91dc2b72a85683d69
Sebas15897 <sebastiancontreras15897@gmail.com>
2021-04-09 21:55:00 +0000

    Fix - task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,4 +1,8 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-   html = response.read()
+"""a Python script that fetches https://intranet.hbtn.io/status"""
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as hbtn:
+        status = hbtn.read()
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+          .format(type(status), status, status.decode('utf-8')))
