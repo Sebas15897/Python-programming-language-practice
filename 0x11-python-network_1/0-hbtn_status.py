49538a671da95f8a3f7c3c06e5ac8a316283c77f
Sebastián Contreras <62773619+Sebas15897@users.noreply.github.com>
2021-04-09 17:05:08 -0500

    Fix - 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+""" Script: Fetches a website and show the response
+"""
 import urllib.request
-"""a Python script that fetches https://intranet.hbtn.io/status"""
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as hbtn:
-        status = hbtn.read()
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-          .format(type(status), status, status.decode('utf-8')))
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+
+    print("Body response:\n\
+\t- type: {}\n\
+\t- content: {}\n\
+\t- utf8 content: {}".format(type(html), html, html.decode("utf-8"))))))
